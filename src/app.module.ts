b5f46e017e127f22a5b84614889b36484c13d5b2
winepnp-code <wine.pnprinz99@gmail.com>
2026-03-23 14:58:24 +0700

    Squashed commit of the following:
    commit c47a520479885f874d0d1c0e18032759e241856c
    Author: winepnp-code <wine.pnprinz99@gmail.com>
    Date:   Mon Mar 23 14:25:42 2026 +0700
    
        feat(auth):workspaces

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, WorkspacesModule],
   controllers: [AppController],
   providers: [AppService],
 })
