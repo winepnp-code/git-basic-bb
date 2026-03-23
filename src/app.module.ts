4b2f86ba43fca6de9b776f6dea7712be89a5475a
winepnp-code <wine.pnprinz99@gmail.com>
2026-03-23 15:26:57 +0700

    feat(columns): generate columns resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
 
 @Module({
-  imports: [AuthModule, UserModule, WorkspacesModule, BoardsModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    WorkspacesModule,
+    BoardsModule,
+    ColumnsModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
