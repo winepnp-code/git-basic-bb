b2c7c3c1b65e465b4e545c593f0aa0e0b0f2fade
winepnp-code <wine.pnprinz99@gmail.com>
2026-03-23 15:25:13 +0700

    feat(boards): generate boards resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [AuthModule, UserModule, WorkspacesModule],
+  imports: [AuthModule, UserModule, WorkspacesModule, BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
